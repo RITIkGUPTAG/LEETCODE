e17bb241ba35f675ef72c55f6eaa42cf612195ff
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2024-11-24 23:57:45 +0530

    Time: 6 ms (91.07%), Space: 54.9 MB (75.42%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -1,29 +1,26 @@
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        int minVal = Integer.MAX_VALUE; // Smallest absolute value in the matrix
-        int negativeCount = 0; // Count of negative numbers
-        long totalSum = 0; // Total sum of absolute values
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int absValue = Math.abs(matrix[i][j]);
-                totalSum += absValue;
-                minVal = Math.min(minVal, absValue); // Track the smallest absolute value
-                if (matrix[i][j] < 0) {
-                    negativeCount++; // Count negative numbers
-                }
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int minVal = Integer.MAX_VALUE;
+        int count=0;
+        long sum=0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int absVal= Math.abs(matrix[i][j]);
+                sum +=absVal;
+      minVal=Math.min(minVal,absVal);
+      if(matrix[i][j]<0){
+        count++;
+      }
             }
-        }
-
-        // If there are an even number of negatives, all can be flipped to positives
-        if (negativeCount % 2 == 0) {
-            return totalSum;
-        } else {
-            // If odd, subtract twice the smallest absolute value to maximize sum
-            return totalSum - 2L * minVal;
-        }
+            }
+            if(count%2==0){
+                return (long)sum;
+            }else{
+                return (long)sum-2*minVal;
+            }
+        
     }
 }
+
