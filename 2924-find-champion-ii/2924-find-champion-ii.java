97bbad865d2ca31b498cf13f46543976ddddc443
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2024-11-26 22:55:52 +0530

    Time: 1 ms (100.00%), Space: 46 MB (25.00%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.java
+++ 2924-find-champion-ii/2924-find-champion-ii.java
@@ -5,12 +5,13 @@ class Solution {
         int[] indegree = new int[n];
 
         // Calculate in-degrees for all nodes
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            // u --> v
-            indegree[v]++;
-        }
+       for (int i = 0; i < edges.length; i++) {
+    int u = edges[i][0];
+    int v = edges[i][1];
+    // u --> v
+    indegree[v]++;
+}
+
 
         int champ = -1;  // To store the potential champion
         int count = 0;   // To count nodes with in-degree 0
