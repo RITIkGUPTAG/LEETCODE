942bc64f25f3ceeb17fa8015042fbf6fcf956ec5
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2024-12-04 23:28:40 +0530

    Time: 6 ms (44.63%), Space: 45.1 MB (6.29%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int j=0;
+        if(s.length()!=t.length()){
+            return false;
+        }
+        
+        
+          char tem[] = s.toCharArray();
+        Arrays.sort(tem);
+   String n= new String(tem); 
+        
+        
+        char temp[] = t.toCharArray();
+        Arrays.sort(temp);
+        String m=new String(temp);
+        
+        
+        for(int i=0; i<n.length();i++){
+            if(n.charAt(i)==m.charAt(j)){
+                j++;
+            }else{
+            return false;
+        }
+        }
+        return true;
+    }
+}
