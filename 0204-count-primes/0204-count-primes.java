bcba1b0cc42c97cc35ec8d24e90b947e01bb63d2
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2024-12-21 13:00:20 +0530

    Time: 90 ms (90.57%), Space: 49.2 MB (44.25%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,28 +1,23 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) {
-            return 0;
-        }
-
+        if (n < 2) return 0;
+        
+int limit =(int)Math.sqrt(n);
         // Create a boolean array to mark non-prime numbers
         boolean[] isPrime = new boolean[n];
-        for (int i = 2; i < n; i++) {
-            isPrime[i] = true;
-        }
-
-        // Sieve of Eratosthenes
-        for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
+        for (int i = 2; i <= limit; i++) {
+           if( isPrime[i] ==false){
                 for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
+                    isPrime[j] = true;;
                 }
             }
+        
         }
 
         // Count prime numbers
         int count = 0;
         for (int i = 2; i < n; i++) {
-            if (isPrime[i]) {
+            if (isPrime[i]==false) {
                 count++;
             }
         }
