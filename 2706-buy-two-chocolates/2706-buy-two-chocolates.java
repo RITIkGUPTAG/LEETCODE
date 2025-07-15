28114c7b0c0acd66483b4587a9ffe4c0c9d87458
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2025-07-15 23:07:30 +0530

    Update 2706-buy-two-chocolates.java

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -5,7 +5,27 @@ class Solution {
        if(sum<=money){
         return (money-sum);
        }else{
-        return money;
+        return money;     //Time Comlexity=O(nlogn)
        }
     }
 }
+// class Solution {
+//     public int buyChoco(int[] prices, int money) {
+//         int min1 = Integer.MAX_VALUE;
+//         int min2 = Integer.MAX_VALUE;                     //Time Complexity=O(n)
+//         for (int i = 0; i < prices.length; i++) {
+//             if (min1 > prices[i]) {
+//                 min2 = min1;
+//                 min1 = prices[i];
+//             } else if (prices[i] < min2) {
+//                 min2 = prices[i];
+//             }
+//         }
+//         int sum = min1 + min2;
+//         if (money >= sum) {
+//             return money - sum;
+//         } else {
+//             return money;
+//         }
+//     }
+// }
