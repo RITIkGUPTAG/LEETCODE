ab7d4c9d2207cc6ed6dfcabd70064571f5623853
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2025-07-17 22:11:51 +0530

    Update 0242-valid-anagram.java

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,18 +3,13 @@ class Solution {
         int j=0;
         if(s.length()!=t.length()){
             return false;
-        }
-        
+        }                                          //Time Complexity=O(nlogn)
           char tem[] = s.toCharArray();
         Arrays.sort(tem);
-   String n= new String(tem); 
-        
-        
+   String n= new String(tem);       
         char temp[] = t.toCharArray();
         Arrays.sort(temp);
         String m=new String(temp);
-        
-        
         for(int i=0; i<n.length();i++){
             if(n.charAt(i)==m.charAt(j)){
                 j++;
@@ -25,3 +20,27 @@ class Solution {
         return true;
     }
 }
+
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length()){
+//             return false;
+//         }                                   //Time Complexity=O(n)
+//         int arr[]=new int[26];
+//         for(int i=0; i<s.length(); i++){
+//             int idx=s.charAt(i)-'a';
+//             arr[idx] +=1;
+//         }
+//          for(int i=0; i<t.length(); i++){
+//             int idx=t.charAt(i)-'a';
+//             arr[idx] -=1;
+//         }
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i]!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
