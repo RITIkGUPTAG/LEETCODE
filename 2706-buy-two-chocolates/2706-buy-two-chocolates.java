fc2ed89f14c7dcd6e6dde37840fc0320b01b1d84
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2025-08-12 13:28:41 +0530

    Time: 1 ms (100%), Space: 44.4 MB (68.51%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,31 +1,22 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int sum=prices[0]+prices[1];
-       if(sum<=money){
-        return (money-sum);
-       }else{
-        return money;     //Time Comlexity=O(nlogn)
-       }
+        int min1 = Integer.MAX_VALUE;
+        int min2  = Integer.MAX_VALUE;
+        for(int i =0; i<prices.length;i++){
+            if(min1> prices[i]){
+                min2 = min1;
+                min1 = prices[i];
+            }
+            else if(prices[i]<min2){
+                min2 = prices[i];
+            }
+        }
+        int sum = min1+min2;
+        if(money >= sum){
+            return money -sum;
+        }
+        else{
+            return money;
+        }
     }
 }
-// class Solution {
-//     public int buyChoco(int[] prices, int money) {
-//         int min1 = Integer.MAX_VALUE;
-//         int min2 = Integer.MAX_VALUE;                     //Time Complexity=O(n)
-//         for (int i = 0; i < prices.length; i++) {
-//             if (min1 > prices[i]) {
-//                 min2 = min1;
-//                 min1 = prices[i];
-//             } else if (prices[i] < min2) {
-//                 min2 = prices[i];
-//             }
-//         }
-//         int sum = min1 + min2;
-//         if (money >= sum) {
-//             return money - sum;
-//         } else {
-//             return money;
-//         }
-//     }
-// }
