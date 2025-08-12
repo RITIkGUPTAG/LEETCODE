842da7c1a501eaa7701784233948371aabf30874
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2025-08-12 14:17:36 +0530

    Time: 6 ms (9.73%), Space: 43.3 MB (81.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int arr[]= new int[nums1.length];
-        Arrays.fill(arr,-1);
+        int arr[]=new int[nums1.length];
         for(int i=0; i<nums1.length; i++){
-            for(int j=0; j<nums2.length; j++){
-                if(nums1[i]==nums2[j]){
-                    for(int k=j+1; k<nums2.length; k++){
-                        if(nums2[j]<nums2[k]){
-                            arr[i]=nums2[k];
-                            break;
-                        }
-                    }
+           for(int j=0; j<nums2.length; j++){
+            if(nums1[i]==nums2[j]){
+                for(int k=j; k<nums2.length; k++){
+                   if(nums2[j]<nums2[k]){
+                    arr[i]=nums2[k];
+                    break;
+                   }
+                   else{
+                    arr[i]=-1;
+                   }
                 }
             }
+           }
         }
         return arr;
     }
