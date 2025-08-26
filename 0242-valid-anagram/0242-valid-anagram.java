f1dbed8dd6a81445b779ddc3f35f95b6ee82a1d8
RITIK GUPTA <166696253+RITIkGUPTAG@users.noreply.github.com>
2025-08-26 14:44:57 +0530

    Time: 4 ms (77.24%), Space: 43.3 MB (70.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,18 +5,17 @@ class Solution {
         }
            int arr[]= new int[26];
            Arrays.fill(arr,0);
-           int arr1[]= new int[26];
-           Arrays.fill(arr1,0);
-           for(int i=0; i<s.length()-1; i++){
+           
+           for(int i=0; i<s.length(); i++){
             int idx=s.charAt(i)-'a';
             arr[idx]++;
            }
-           for(int i=0; i<t.length()-1; i++){
+           for(int i=0; i<t.length(); i++){
             int idx=t.charAt(i)-'a';
-            arr1[idx]++;
+            arr[idx]--;
            }
-            for(int i=0; i<s.length(); i++){
-           if(arr[i]!=arr1[i]){
+            for(int i=0; i<arr.length; i++){
+           if(arr[i]!=0){
             return false;
            }
            }
